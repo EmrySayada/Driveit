f408b885a9eb9fcf69eb8bfde884e108153a87d1
Emry Sayada <emrin276@outlook.com>
2025-03-10 09:35:27 +0200

    added notifications for lessons for both teachers and pupils.

--- app/src/main/java/com/example/driveit/LessonFragment.java
+++ app/src/main/java/com/example/driveit/LessonFragment.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LessonFragment extends Fragment {
+    Button startLessonBtn, callPupilBtn;
+    TextView lessontimeTv, pupilUsername;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
