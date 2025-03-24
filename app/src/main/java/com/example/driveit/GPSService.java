061ecd0fde00d306d085f2e98e3a8144323425ad
EmrySayada <emrin276@outlook.com>
2025-03-24 09:40:55 +0200

    started working on the gps logic

--- app/src/main/java/com/example/driveit/GPSService.java
+++ app/src/main/java/com/example/driveit/GPSService.java
@@ -1,15 +1,26 @@
 package com.example.driveit;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 public class GPSService extends Service implements LocationListener {
+    DBHelper mydb;
+    SQLiteDatabase sqdb;
+    String provider;
+    LocationManager locationManager;
+    Location location;
     public GPSService() {
     }
 
@@ -21,6 +32,19 @@ public class GPSService extends Service implements LocationListener {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Criteria criteria = new Criteria();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+        locationManager.requestLocationUpdates(provider, 2000, 1, this);
         return super.onStartCommand(intent, flags, startId);
     }
 
